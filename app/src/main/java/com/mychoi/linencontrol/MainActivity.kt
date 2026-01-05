05f141d01f8ad0c860abcc37323faac860ec1ebb
choiMY <thskan123439@gmail.com>
2026-01-05 21:59:03 +0900

    feat: main activity content MainScreen으로 변경

--- app/src/main/java/com/mychoi/linencontrol/MainActivity.kt
+++ app/src/main/java/com/mychoi/linencontrol/MainActivity.kt
@@ -11,20 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mychoi.linencontrol.ui.main.MainScreen
 import com.mychoi.linencontrol.ui.theme.LinenControlTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LinenControlTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
