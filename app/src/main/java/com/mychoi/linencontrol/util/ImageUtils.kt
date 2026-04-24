be08a1023e78f55a5bd5bc47f365b0a2b31914f3
choiMY <thskan123439@gmail.com>
2026-04-24 21:46:19 +0900

    feat: URI/Bitmap을 Base 64 변환

---
+++ app/src/main/java/com/mychoi/linencontrol/util/ImageUtils.kt
@@ -0,0 +1,27 @@
+package com.mychoi.linencontrol.util
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Base64
+import java.io.ByteArrayOutputStream
+
+object ImageUtils {
+    fun bitmapToBase64(bitmap: Bitmap): String {
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outputStream)
+        return Base64.encodeToString(outputStream.toByteArray(), Base64.NO_WRAP)
+    }
+
+    fun uriToBitmap(context: Context, uri: Uri): Bitmap? = runCatching {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
+        } else {
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+        }
+    }.getOrNull()
+}
